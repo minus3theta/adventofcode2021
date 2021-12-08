e69fcadab64528220d9e344bd0e3974d80543817
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-12-08 20:36:39 +0900

    Add 08

---
+++ src/bin/08a.rs
@@ -0,0 +1,62 @@
+use std::collections::BTreeSet;
+
+use adventofcode2021::get_stdin;
+use itertools::Itertools;
+use once_cell::sync::Lazy;
+
+static DIGITS: Lazy<Vec<Digit>> = Lazy::new(|| {
+    [
+        "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg",
+    ]
+    .into_iter()
+    .map(str_to_index)
+    .collect()
+});
+
+type Pat = Vec<usize>;
+type PatRef<'a> = &'a [usize];
+type Digit = BTreeSet<usize>;
+
+fn translate(pat: PatRef, digit: &Digit) -> Option<usize> {
+    let t: Digit = digit.iter().map(|&d| pat[d]).collect();
+    DIGITS.iter().find_position(|&d| d == &t).map(|(i, _)| i)
+}
+
+fn find_pat(display: &[Digit]) -> Pat {
+    for pat in (0..7).permutations(7) {
+        if display.iter().all(|d| translate(&pat, d).is_some()) {
+            return pat;
+        }
+    }
+    unreachable!()
+}
+
+fn alpha_to_index(c: char) -> usize {
+    (c as u32 - 'a' as u32) as usize
+}
+
+fn str_to_index(s: &str) -> BTreeSet<usize> {
+    s.chars().map(alpha_to_index).collect()
+}
+
+fn parse_display(display: &str) -> Vec<Digit> {
+    display.split_ascii_whitespace().map(str_to_index).collect()
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = get_stdin()?;
+    let mut popl = vec![0; 10];
+    for line in input.lines() {
+        let line = line.split(" | ").collect_vec();
+        let display = parse_display(line[0]);
+        let pat = find_pat(&display);
+        let ret = parse_display(line[1]);
+        for r in &ret {
+            let r = translate(&pat, r).unwrap();
+            popl[r] += 1;
+        }
+    }
+    dbg!(popl[1] + popl[4] + popl[7] + popl[8]);
+
+    Ok(())
+}
