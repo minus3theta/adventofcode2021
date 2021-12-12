26a58f9c53d547d969770541c51c0bc9c3470a48
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-12-12 19:58:57 +0900

    Add 12

---
+++ src/bin/12a.rs
@@ -0,0 +1,98 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+use adventofcode2021::get_stdin;
+use itertools::Itertools;
+
+fn dfs(adj: &BTreeMap<&str, BTreeSet<&str>>, v: &str, visited: BTreeSet<&str>) -> usize {
+    let mut ret = 0;
+    for &u in &adj[v] {
+        if u == "start" {
+            continue;
+        }
+        if u == "end" {
+            ret += 1;
+        } else {
+            let u_small = u.chars().all(|c| c.is_ascii_lowercase());
+            if u_small && visited.contains(u) {
+                continue;
+            }
+            let mut new_visited = visited.clone();
+            new_visited.insert(u);
+            ret += dfs(adj, u, new_visited);
+        }
+    }
+    ret
+}
+
+fn solve(input: &str) -> usize {
+    let mut adj = BTreeMap::new();
+    for line in input.lines() {
+        let (a, b) = line.split('-').collect_tuple().unwrap();
+        adj.entry(a).or_insert_with(BTreeSet::new).insert(b);
+        adj.entry(b).or_insert_with(BTreeSet::new).insert(a);
+    }
+    dfs(&adj, "start", BTreeSet::new())
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = get_stdin()?;
+
+    println!("{}", solve(&input));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_1() {
+        let input = "start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end";
+        assert_eq!(solve(input), 10);
+    }
+
+    #[test]
+    fn sample_2() {
+        let input = "dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc";
+        assert_eq!(solve(input), 19);
+    }
+
+    #[test]
+    fn sample_3() {
+        let input = "fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW";
+        assert_eq!(solve(input), 226);
+    }
+}
